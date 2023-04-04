a8857e2056c33aaff38f2056ea487967edb0ba45
Oscar Simon <2130484@etu.cegepjonquiere.ca>
2023-04-04 17:12:31 -0400

    Atelier Net

---
+++ Vetements/Controllers/MVCVetement.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vetements.Controllers
+{
+    public class MVCVetement : Controller
+    {
+        // GET: MVCVetement
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: MVCVetement/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MVCVetement/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MVCVetement/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MVCVetement/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MVCVetement/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MVCVetement/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MVCVetement/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
---
+++ Vetements/Data/VetementsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCVetement.Models;
+
+namespace Vetements.Data
+{
+    public class VetementsContext : DbContext
+    {
+        public VetementsContext (DbContextOptions<VetementsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCVetement.Models.Vetement> Vetement { get; set; } = default!;
+    }
+}
